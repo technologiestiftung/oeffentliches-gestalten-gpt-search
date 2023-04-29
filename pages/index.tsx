0be8dfe1e71cac432b21cdc359744f0c118ba893
ff6347 <hash@ff6347.email>
2023-04-29 09:15:55 +0200

    chore: Remove aliased paths
    since they always annoy me and break in storybook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
-import { SearchDialog } from "@/components/SearchDialog";
+import styles from "../styles/Home.module.css";
+import { SearchDialog } from "../components/SearchDialog";
 
 export default function Home() {
 	return (
