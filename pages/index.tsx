ddc7b452f660ed4a4600391c5f0e07343dd566fb
ff6347 <hash@ff6347.email>
2023-05-02 11:14:48 +0200

    try with uppercase

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 	console.log("in getServerSideProps", context.res.getHeaders());
-	const csrfToken = context.res.getHeader("x-csrf-token") ?? "missing";
+	const csrfToken = context.res.getHeader("X-CSRF-Token") ?? "missing";
 	if (csrfToken === "missing") {
 		throw new Error("Invalid CSRF token");
 	}
