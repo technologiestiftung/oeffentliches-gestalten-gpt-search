453fa3a6cddba2557b09041c09e4a31e542cc28a
ff6347 <hash@ff6347.email>
2023-05-03 20:56:36 +0200

    test: Fix home test

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -6,6 +6,6 @@ test("home", () => {
 	render(<Home />);
 	const main = within(screen.getByRole("main"));
 	expect(
-		main.getByRole("heading", { level: 1, name: /Handbuch GPT Search/i })
+		main.getByRole("heading", { level: 1, name: /Handbuch GPT Suche/i })
 	).toBeDefined();
 });
