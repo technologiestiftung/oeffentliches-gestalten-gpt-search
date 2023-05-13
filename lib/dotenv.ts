a8e905fa048229804701c27b5e7550ee34918f7c
ff6347 <hash@ff6347.email>
2023-05-13 13:32:01 +0200

    chore: Safeguard against missing variables

--- lib/dotenv.ts
+++ lib/dotenv.ts
@@ -1,7 +1,11 @@
-const NODE_ENV = process.env.NODE_ENV ?? "development";
+import { EnvError } from "./errors";
+
 // frontend
 const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const NEXT_PUBLIC_HANDBUCH_URL =
+	process.env.NEXT_PUBLIC_HANDBUCH_URL ??
+	"https://www.oeffentliches-gestalten.de";
 // backend
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -9,10 +13,34 @@ const OPENAI_KEY = process.env.OPENAI_KEY;
 const MDX_DOCS_PATH = process.env.MDX_DOCS_PATH;
 const UPSTASH_REST_API_DOMAIN = process.env.UPSTASH_REST_API_DOMAIN;
 const UPSTASH_REST_API_TOKEN = process.env.UPSTASH_REST_API_TOKEN;
+// no need to test these since they have defaults
 const OPENAI_MODEL = process.env.OPENAI_MODEL ?? "gpt-3.5-turbo";
-const NEXT_PUBLIC_HANDBUCH_URL =
-	process.env.NEXT_PUBLIC_HANDBUCH_URL ??
-	"https://www.oeffentliches-gestalten.de";
+const NODE_ENV = process.env.NODE_ENV ?? "development";
+
+if (NEXT_PUBLIC_SUPABASE_ANON_KEY === undefined) {
+	throw new EnvError("NEXT_PUBLIC_SUPABASE_ANON_KEY");
+}
+if (NEXT_PUBLIC_SUPABASE_URL === undefined) {
+	throw new EnvError("NEXT_PUBLIC_SUPABASE_URL");
+}
+if (SUPABASE_SERVICE_ROLE_KEY === undefined) {
+	throw new EnvError("SUPABASE_SERVICE_ROLE_KEY");
+}
+if (JWT_SECRET === undefined) {
+	throw new EnvError("JWT_SECRET");
+}
+if (OPENAI_KEY === undefined) {
+	throw new EnvError("OPENAI_KEY");
+}
+if (MDX_DOCS_PATH === undefined) {
+	throw new EnvError("MDX_DOCS_PATH");
+}
+if (UPSTASH_REST_API_DOMAIN === undefined) {
+	throw new EnvError("UPSTASH_REST_API_DOMAIN");
+}
+if (UPSTASH_REST_API_TOKEN === undefined) {
+	throw new EnvError("UPSTASH_REST_API_TOKEN");
+}
 
 export {
 	NODE_ENV,
