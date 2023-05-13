18003e90d31b1e26933df4cf770f2e8779fc2c4c
ff6347 <hash@ff6347.email>
2023-05-13 13:36:27 +0200

    chore: Add env related error type

--- lib/errors.ts
+++ lib/errors.ts
@@ -7,4 +7,11 @@ export class AuthError extends Error {}
 
 export class UserError extends ApplicationError {}
 
-export class AuthError extends Error {}
+/**
+ * Does pretty hadnling of messages
+ */
+export class EnvError extends Error {
+	constructor(message: string) {
+		super(`Env variable "${message}" is not defined`);
+	}
+}
