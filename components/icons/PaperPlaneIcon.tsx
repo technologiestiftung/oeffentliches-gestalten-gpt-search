7f1c7a54f1a71cb08f5027185cdd6939a3881207
Raphael Arce <raphael.arce@ts.berlin>
2023-05-24 08:09:59 +0200

    feat: added color change on hover for paper plane
    Signed-off-by: Raphael Arce <raphael.arce@ts.berlin>

--- components/icons/PaperPlaneIcon.tsx
+++ components/icons/PaperPlaneIcon.tsx
@@ -8,11 +8,9 @@ export const PaperPlaneIcon = () => {
 			height="24"
 			viewBox="0 0 24 24"
 			fill="none"
-			stroke="grey"
-			strokeWidth="1"
 			strokeLinecap="round"
 			strokeLinejoin="round"
-			className="feather feather-send"
+			className="feather feather-send stroke-grey-400 stroke-1 hover:stroke-magenta-500 hover:stroke-2"
 		>
 			<line x1="22" y1="2" x2="11" y2="13"></line>
 			<polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
